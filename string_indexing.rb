8217e9879ff72456199ddb36519a2f7acc4f5a14
Noah Carlson <noahjc0501@gmail.com>
2024-03-03 17:41:57 -0500

    adding scripts working with strings

---
+++ string_indexing.rb
@@ -0,0 +1,7 @@
+sentence = "Hello, how are you today?"
+puts sentence[7]
+puts sentence[0,5]
+
+sentance2 = "Hello, how are you today? Hello, how are you today?"
+
+puts sentance2.gsub("Hello", "Hi");
